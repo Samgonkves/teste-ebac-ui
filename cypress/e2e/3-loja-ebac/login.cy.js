20b99cb6d46110a75a47f95b5c9dc491ca52718d
Sara Gonçalves <saramariags@gmail.com>
2025-05-28 19:31:27 -0300

    Segundo commit

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,11 +2,35 @@
 
 describe('Funcionalidade: Login', () => {
 
-    it('Fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Fazer login com sucesso', () => {
         cy.get('#username').type('saramariags@gmail.com')
         cy.get('#password').type('tEST$OFT5')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, saramariags')
     })
+
+    it('Exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('saramaria@gmail.com')
+        cy.get('#password').type('tEST$OFT5')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error > li').should('exist')
+    });
+
+    it('Exibir senha inválida', () => {
+        cy.get('#username').type('saramariags@gmail.com')
+        cy.get('#password').type('tEST$T5')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Perdeu a senha?')
+        cy.get('.woocommerce-error > li').should('exist')
+        
+    });
 })
