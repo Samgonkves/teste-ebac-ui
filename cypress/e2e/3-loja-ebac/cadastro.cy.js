9be3021cd21aa1c5f34d0a0f3a2f80c1a282a698
Sara Gonçalves <saramariags@gmail.com>
2025-05-28 21:45:44 -0300

    Terceiro commit

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,39 @@
+///<reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Completar o cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste@123')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','modificados com sucesso')
+    });
+
+    it.only('Completar o cadastro variáveis', () => {
+        var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobren = faker.person.lastName()
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('teste@123')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobren)
+        cy.wait(5000)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','modificados com sucesso')
+        
+});
+
+})
